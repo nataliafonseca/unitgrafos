0dfffc212e12dd147c4d939ff80b6131a38b32fc
nataliabf <53270236+nataliabf@users.noreply.github.com>
2020-02-03 09:12:08 -0300

    implementação inicial da representação do grafo

--- grafo.py
+++ grafo.py
@@ -1,8 +1,19 @@
-quantidade_vetores = int(input("Informe a quantidade de vetores do grafo: "))
+# quantidade_vetores = int(input("Informe a quantidade de vetores do grafo: "))
+quantidade_vetores = 6
 
 grafo = []
 for i in range(quantidade_vetores):
     grafo.append([0] * quantidade_vetores)
+
+# arestas = informe as arestas do grafo, separe cada os vertices ligados
+# por virgulas e os pares de vertices por ponto e virgula.
+arestas = "0,1;1,2;1,3;2,4;2,5"
+arestas = arestas.split(";")
+
+for par in arestas:
+    i, j = par.split(",")
+    i, j = int(i.strip()), int(j.strip())
+    grafo[i][j] = 1
 
 for i in range(quantidade_vetores):
     print(grafo[i])
