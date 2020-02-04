54bd94a63203a7b9be090b76cc2144fd0bdcf6b3
nataliabf <53270236+nataliabf@users.noreply.github.com>
2020-02-04 09:09:46 -0300

    Mudança do formato de entrada das arestas.

--- matriz_adjacencia.py
+++ matriz_adjacencia.py
@@ -6,13 +6,13 @@ for i in range(quantidade_vetores):
     grafo.append([0] * quantidade_vetores)
 
 # arestas = input("Informe as arestas do grafo, separe cada os vertices "
-#                 "ligados por virgulas e os pares de vertices por "
+#                 "ligados por traços e os pares de vertices por "
 #                 "ponto e virgula.")
-arestas = "0,1;1,2;1,3;2,4;2,5"
+arestas = "0-1;1-2;1-3;2-4;2-5"
 arestas = arestas.split(";")
 
 for par in arestas:
-    i, j = par.split(",")
+    i, j = par.split("-")
     i, j = int(i.strip()), int(j.strip())
     grafo[i][j] = 1
 
