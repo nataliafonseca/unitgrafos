977566ea44efd02330c6de8e10874e51c063e11b
Natália B. Fonseca <nataliabf@outlook.com>
2020-02-27 09:45:25 -0300

    Adição da obtenção de menor caminho à interface inicial do sistema.

--- lib/interface.py
+++ lib/interface.py
@@ -1,4 +1,5 @@
-from colorama import init as color, Fore
+from model.grafo import *
+
 color()
 
 
@@ -34,3 +35,21 @@ def menu(lista):
     print(linha())
     opc = leiaint("Sua opção: ")
     return opc
+
+
+def teste_grafo_definido(grafo):
+    digrafo = False
+    valorado = False
+    q_vertices = 12
+    arestas = "1-2, 1-3, 2-3, 2-5, 2-6, 3-4, 3-6, 4-7, 5-6, 5-9, 5-10, 6-7," \
+              "6-10, 6-11, 7-8, 7-12, 8-12, 9-10, 10-11"
+    grafo_exemplo = Grafo(digrafo, valorado, q_vertices, arestas)
+    if not grafo:
+        print(Fore.RED + "ATENÇÃO! Você ainda não definiu um grafo, "
+                         "serão impressas as informações correspondentes "
+                         "ao grafo representado em 'exemplo/simples.png'. "
+                         "Para adicionar seu proprio grafo, selecione a "
+                         "opção 1."
+              + Fore.RESET)
+        return grafo_exemplo
+    return grafo
--- sistema.py
+++ sistema.py
@@ -1,16 +1,7 @@
-from model.grafo import *
+from lib.interface import *
 from time import sleep
 
-# Grafo de Exemplo - será utilizado se o usuário não cadastrar um grafo.
-digrafo = False
-valorado = False
-q_vertices = 12
-arestas = "1-2, 1-3, 2-3, 2-5, 2-6, 3-4, 3-6, 4-7, 5-6, 5-9, 5-10, 6-7," \
-          "6-10, 6-11, 7-8, 7-12, 8-12, 9-10, 10-11"
-
 grafo = None
-grafo_exemplo = Grafo(digrafo, valorado, q_vertices, arestas)
-
 
 while True:
     resposta = menu(["Definir Grafo",
@@ -18,6 +9,8 @@ while True:
                      "Imprimir Informações do Grafo",
                      "Imprimir Matriz de Adjacencia",
                      "Imprimir Estrutura de Adjacencia",
+                     "Obter Menor Caminho entre Dois Vértices",
+                     "Obter Menores Caminhos a partir de um Vértice",
                      "Sair do sistema"])
 
     if resposta == 1:
@@ -40,40 +33,43 @@ while True:
 
     elif resposta == 3:
         print()
-        cabecalho("Opção 2 - Imprimir Informações do Grafo")
+        cabecalho("Opção 3 - Imprimir Informações do Grafo")
+        grafo = teste_grafo_definido(grafo)
         grafo.imprimir_informacoes()
         print()
 
     elif resposta == 4:
         print()
-        cabecalho("Opção 3 - Imprimir Matriz de Adjacencia")
-        if not grafo:
-            print(Fore.RED + "ATENÇÃO! Você ainda não definiu um grafo, "
-                             "será impressa a matriz correspondente ao "
-                             "grafo representado em 'exemplo/simples.png'. "
-                             "Para adicionar seu proprio grafo, selecione a "
-                             "opção 1."
-                  + Fore.RESET)
-            grafo = grafo_exemplo
-
+        cabecalho("Opção 4 - Imprimir Matriz de Adjacencia")
+        grafo = teste_grafo_definido(grafo)
         grafo.imprimir_matriz_adjacencia()
         print()
 
     elif resposta == 5:
         print()
-        cabecalho("Opção 4 - Imprimir Estrutura de Adjacencia")
-        if not grafo:
-            print(Fore.RED + "ATENÇÃO! Você ainda não definiu um grafo, "
-                             "será impressa a estrutura correspondente ao "
-                             "grafo representado em 'exemplo/simples.png'. "
-                             "Para adicionar seu proprio grafo, selecione a "
-                             "opção 1."
-                  + Fore.RESET)
-            grafo = grafo_exemplo
+        cabecalho("Opção 5 - Imprimir Estrutura de Adjacencia")
+        grafo = teste_grafo_definido(grafo)
         grafo.imprimir_estrutura_adjacencia()
         print()
 
     elif resposta == 6:
+        print()
+        cabecalho("Opção 6 - Obter Menor Caminho entre Dois Vértices")
+        grafo = teste_grafo_definido(grafo)
+        origem = int(input("Informe o vértice inicial: "))
+        destino = int(input("Informe o vértice final: "))
+        print(grafo.get_menor_caminho(origem, destino))
+        print()
+
+    elif resposta == 7:
+        print()
+        cabecalho("Opção 7 - Obter Menores Caminhos a partir de um Vértice")
+        grafo = teste_grafo_definido(grafo)
+        origem = int(input("Informe o vértice inicial: "))
+        print(grafo.get_menor_caminho(origem))
+        print()
+
+    elif resposta == 8:
         print()
         cabecalho("Saindo do sistema... Até logo!")
         sleep(1)
@@ -81,6 +77,6 @@ while True:
 
     else:
         print(Fore.RED + "ERRO: Por favor, digite um número inteiro entre "
-                         "1 e 6" + Fore.RESET)
+                         "1 e 8" + Fore.RESET)
 
     sleep(1)
